7128546459455303a7b888fd5386137932019378
Aboeleneen <aboeleneen.98@yahoo.com>
2019-04-13 14:34:43 +0200

    third task

--- machine-learning-ex5/ex5/learningCurve.m
+++ machine-learning-ex5/ex5/learningCurve.m
@@ -53,7 +53,12 @@ error_val   = zeros(m, 1);
 
 % ---------------------- Sample Solution ----------------------
 
-
+[theta] = trainLinearReg([ones(m, 1) X], y, lambda);
+for i =1:m
+       [theta] = trainLinearReg(X(1:i,:), y(1:i), lambda);
+       error_train(i)=linearRegCostFunction(X(1:i,:),y(1:i),theta,0);
+       error_val(i)=linearRegCostFunction(Xval,yval,theta,0);
+end
 
 
 
