d4bf814284f2597bf153cca6e5d465fa9c2de196
Aboeleneen <aboeleneen.98@yahoo.com>
2019-05-03 15:30:35 +0200

    second task ex6 done :)

--- machine-learning-ex6/ex6/dataset3Params.m
+++ machine-learning-ex6/ex6/dataset3Params.m
@@ -10,7 +10,7 @@ function [C, sigma] = dataset3Params(X, y, Xval, yval)
 % You need to return the following variables correctly.
 C = 1;
 sigma = 0.3;
-
+values = [0.01,0.03, 0.1, 0.3, 1, 3, 10, 30]
 % ====================== YOUR CODE HERE ======================
 % Instructions: Fill in this function to return the optimal C and sigma
 %               learning parameters found using the cross validation set.
@@ -22,6 +22,21 @@ sigma = 0.3;
 %  Note: You can compute the prediction error using 
 %        mean(double(predictions ~= yval))
 %
+model =svmTrain(X,y,C,@(x1, x2) gaussianKernel(x1, x2, sigma));
+predictions = svmPredict(model,Xval);
+MinError = mean(double(predictions ~= yval));
+for i=1:8
+    for j=1:8
+        model =svmTrain(X,y,values(i),@(x1, x2) gaussianKernel(x1, x2, values(j)));
+        predictions = svmPredict(model,Xval);
+        error = mean(double(predictions ~= yval));
+        if error < MinError 
+            MinError = error ;
+            C=values(i);
+            sigma=values(j);
+        end
+    end
+end
 
 
 
